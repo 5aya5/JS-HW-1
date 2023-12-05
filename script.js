9615ebc363d072ec3bc3379d90a2a788b9f006f8
Safoyev <saidsaid01sm@gmail.com>
2023-12-05 23:36:07 +0500

    JS-1

---
+++ script.js
@@ -0,0 +1,43 @@
+
+var name = prompt("Введи имя: ")
+
+var year = prompt("Введи возраст: ")
+
+var addition = prompt("10 + 10 = ?")
+
+var subtraction = prompt("10 - 5 = ?")
+
+var multiplication = prompt("10 * 2 = ?")
+
+var division = prompt("10 / 5 = ?")
+
+var divisionWithRemainder = prompt("10 % 3 = ?")
+
+
+console.log("Ты " + name + "!");
+
+console.log("И тебе "+ year + " лет" + "!");
+
+console.log("-----------------------------");
+
+console.log("Твой ответ: " + "10 + 10 = " + addition + ". Правильный ответ: " + (10 + 10) + "!");
+
+console.log("-----------------------------");
+
+console.log("Твой ответ: " + "10 - 5 = " + subtraction + ". Правильный ответ: " + (10 - 5) + "!");
+
+console.log("-----------------------------");
+
+console.log("Твой ответ: " + "10 * 2 = " + multiplication + ". Правильный ответ: " + (10 * 2) + "!");
+
+console.log("-----------------------------");
+
+console.log("Твой ответ: " + "10 / 5 = " + division + ". Правильный ответ: " + (10 / 5) + "!");
+
+console.log("-----------------------------");
+
+console.log("Твой ответ: " + "10 % 3 = " + divisionWithRemainder + ". Правильный ответ: " + (10 % 3) + "!");
+
+console.log("-----------------------------");
+
+console.log("Спасибо за внимание!");
